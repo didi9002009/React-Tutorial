231bca95462db9b1d6e6d4c69c1da0c9d98d3f4e
didi9002009 <53850889+didi9002009@users.noreply.github.com>
2020-01-28 16:44:18 +1100

    Commit

--- src/App.js
+++ src/App.js
@@ -10,14 +10,22 @@ import "./styles.css";
 // import EventBind from "./components/EventBind";
 // import ParentComponent from "./components/ParentComponent";
 // import UserGreeting from "./components/UserGreeting";
-import NameList from "./components/NameList";
+// import NameList from "./components/NameList";
+// import Stylesheet from "./components/Stylesheet";
+import Inline from "./components/Inline";
+import "./appStyles.css";
+import styles from "./appStyles.module.css";
 
 class App extends Component {
   // eslint-disable-next-line no-use-before-define" before the render line in App.js
   render() {
     return (
       <div className="App">
-        <NameList />
+        <h1 className="error">Error</h1>
+        <h1 className={styles.success}>Success</h1>
+        <Inline />
+        {/* <Stylesheet primary={true} /> */}
+        {/* <NameList /> */}
         {/* <UserGreeting /> */}
         {/* <ParentComponent /> */}
         {/* <EventBind /> */}
--- src/components/NameList.js
+++ src/components/NameList.js
@@ -1,39 +1,34 @@
 import React from "react";
-import Person from "./Person";
+// import Person from "./Person";
 
 function NameList() {
-  const persons = [
-    {
-      id: 1,
-      name: "Dan",
-      age: 30,
-      skill: "React"
-    },
-    {
-      id: 2,
-      name: "Jim",
-      age: 25,
-      skill: "Vue"
-    },
-    {
-      id: 3,
-      name: "Kim",
-      age: 33,
-      skill: "Angular"
-    }
-  ];
-  const personList = persons.map(person => <Person person={person} />); //person as a para passed as a prop to the Person compo
-  return <div>{personList}</div>;
-
-  //   <h2>
-  //     I am {person.name}. I am {person.age} years old. I know {person.skill}
-  //   </h2>
-  // ));
-  // return <div>{personList}</div>;
-
-  // const names = ['Dan'. 'Jim', 'Kim']
-  // const nameList = names.map(name => (<h2>{name}</h2>));
-  // return <div>{nameList}</div>;
+  const names = ["Dan", "Jim", "Kim", "Dan"];
+  // const persons = [
+  //   {
+  //     id: 1,
+  //     name: "Dan",
+  //     age: 30,
+  //     skill: "React"
+  //   },
+  //   {
+  //     id: 2,
+  //     name: "Jim",
+  //     age: 25,
+  //     skill: "Vue"
+  //   },
+  //   {
+  //     id: 3,
+  //     name: "Kim",
+  //     age: 33,
+  //     skill: "Angular"
+  //   }
+  // ];
+  const nameList = names.map((name, index) => (
+    <h2 key={index}>
+      {index} {name}
+    </h2>
+  ));
+  return <div>{nameList}</div>;
 }
 
 export default NameList;
