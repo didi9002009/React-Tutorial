740059e326364138dd23dee087b8634f98a25301
didi9002009 <53850889+didi9002009@users.noreply.github.com>
2020-02-03 15:36:26 +1100

    update settings

--- src/components/UserGreeting.js
+++ src/components/UserGreeting.js
@@ -8,26 +8,7 @@ class UserGreeting extends Component {
     };
   }
     render() {
-      return this.state.isLoggedIn && <div>Welcome Dan</div> //mtd 4, also preferable
-
-      // return this.state.isLoggedIn ?    // mtd 3: preferable
-      //  (<div>Welcome Dan</div>) :
-      //   (<div>Welcome Guest</div>)
-
-    // let message                           //mtd 2
-    // if(this.state.isLoggedIn){
-    //   message = <div>Welcome Dan</div>
-    // }else{
-    //    message = <div>Welcome Dan</div>
-    // }
-    
-    // return <div>{message}</div>        //mtd 1
-  //   if (this.state.isLoggedIn) {
-  //     return <div>Welcome Dan</div>;
-  //   } else {
-  //     return <div>Welcome Guest</div>;
-  //   }
-  // }
-}
-
+      return this.state.isLoggedIn && <div>Welcome Dan</div> 
+    }
+  
 export default UserGreeting;
